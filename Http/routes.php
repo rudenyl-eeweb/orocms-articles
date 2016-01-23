1c6a733cf9c0a368b58c53f3adb3fb5e2cc55518
rudenyl-eeweb <rudenyl.eeweb@gmail.com>
2016-01-23 15:21:03 +0800

    Laravel 5.2 support for session middleware

--- Http/routes.php
+++ Http/routes.php
@@ -2,7 +2,7 @@
 /**
  * Routes
  */
-Route::group(['namespace' => 'Modules\Articles\Http\Controllers'], function() {
+Route::group(['namespace' => 'Modules\Articles\Http\Controllers', 'middleware' => ['web']], function() {
     Route::group(['prefix' => 'articles'], function() {
         Route::get('/', ['as' => 'articles.index', 'uses' => 'ArticlesController@index']);
         Route::get('/{slug}', ['as' => 'articles.show', 'uses' => 'ArticlesController@show']);
@@ -12,18 +12,6 @@ Route::group(['namespace' => 'Modules\Articles\Http\Controllers'], function() {
      * Admin routes
      */
     Route::group(['prefix' => 'admin/modules/articles', 'middleware' => config('admin.filter.auth')], function() {
-        // Route::resource('/', AdminController::class, [
-        //     'names' => [
-        //         'index' => 'admin.modules.articles.index',
-        //         'create' => 'admin.modules.articles.create',
-        //         'store' => 'admin.modules.articles.store',
-        //         'show' => 'admin.modules.articles.show',
-        //         'update' => 'admin.modules.articles.update',
-        //         'edit' => 'admin.modules.articles.edit',
-        //         'destroy' => 'admin.modules.articles.destroy'
-        //     ]
-        // ]);
-        
         Route::get('/', ['as' => 'admin.modules.articles.index', 'uses' => 'AdminController@index']);
         Route::get('/create', ['as' => 'admin.modules.articles.create', 'uses' => 'AdminController@create']);
         Route::get('/{article}/edit', ['as' => 'admin.modules.articles.edit', 'uses' => 'AdminController@edit']);
